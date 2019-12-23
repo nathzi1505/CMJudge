071be1e7f648f4a8b88a5441eb7ee81de89b0337
theSYNcoder <papaigd@gmail.com>
2019-12-24 00:33:19 +0530

    dc

---
+++ build/precache-manifest.48c2f96ca94bc7c63060c7807b5c17c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747ec85a154e510f47e77410e59223e8",
+    "url": "/CMJudge/index.html"
+  },
+  {
+    "revision": "9bb52e7a69677ca6d26e",
+    "url": "/CMJudge/static/css/2.ef23f0a5.chunk.css"
+  },
+  {
+    "revision": "8400158a0e0093cb971e",
+    "url": "/CMJudge/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9bb52e7a69677ca6d26e",
+    "url": "/CMJudge/static/js/2.3bd0d479.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/CMJudge/static/js/2.3bd0d479.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8400158a0e0093cb971e",
+    "url": "/CMJudge/static/js/main.2253e9a0.chunk.js"
+  },
+  {
+    "revision": "cd2bea4fc8997e16524f",
+    "url": "/CMJudge/static/js/runtime-main.5c485e16.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CMJudge/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CMJudge/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CMJudge/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CMJudge/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CMJudge/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
